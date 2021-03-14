5fb56a16aed7cc3296e0f344ac45082ffd9ca3a5
skillJackal <79844040+skillJackal@users.noreply.github.com>
2021-03-14 16:30:15 +0100

    some cosmetic adjustments

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,12 @@
 <template>
     <v-app>
-        <center>
-            <v-container fill-height fluid>
-                <v-row align="center"
-                    justify="center">
-                    <v-col>
+        <v-container fill-height>
+            <v-row align="center" justify="center">
+                <v-col cols="12" sm="6" class="text-center">
                     <nuxt />
-                    </v-col>
-                </v-row>
-            </v-container>
-        </center>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-app>
 </template>
 
